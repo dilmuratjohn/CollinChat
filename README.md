43cf5b744f9c809e1143e04821198e9f26def76e
collin <duolikun@ihandysoft.com>
2018-10-10 16:16:58 +0800

    Ep.2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Network Chat Programming
 
 > - Ep.1 Window Builder
+> - Ep.2 The Login Window
--- src/com/dilmuratjohn/ichat/Login.java
+++ src/com/dilmuratjohn/ichat/Login.java
@@ -10,25 +10,60 @@ public class Login extends JFrame {
     private static final long serialVersionUID = 1L;
 
     private JPanel contentPane;
+    private JTextField textName;
+    private JLabel labelName;
+    private JTextField textPassword;
+    private JLabel labelPassword;
 
     public Login() {
 
+        setTitle("Login");
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setSize(300, 500);
+        setLocationRelativeTo(null);
+
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
+        contentPane.setLayout(null);
         setContentPane(contentPane);
+
+        this.addParts();
+
+
+    }
+
+    private void addParts() {
+
+        textName = new JTextField();
+        textName.setBounds(125, 50, 150, 30);
+        textName.setColumns(10);
+
+        textPassword = new JTextField();
+        textPassword.setBounds(125, 100, 150, 30);
+        textPassword.setColumns(10);
+
+        labelName = new JLabel("Name:");
+        labelName.setBounds(30, 50, 70, 30);
+
+        labelPassword = new JLabel("Password:");
+        labelPassword.setBounds(30, 100, 70, 30);
+
+        this.contentPane.add(textName);
+        this.contentPane.add(textPassword);
+        this.contentPane.add(labelName);
+        this.contentPane.add(labelPassword);
+
     }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                try{
+                try {
                     Login frame = new Login();
                     frame.setVisible(true);
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
